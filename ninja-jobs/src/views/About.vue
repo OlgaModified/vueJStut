451cdea6601102ba2aeddbcb8dda15b033eed2ce
OlgaModified <olga@modified.dk>
2021-11-05 13:11:27 +0100

    added some divs with data

--- ninja-jobs/src/views/About.vue
+++ ninja-jobs/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quasi enim quos doloremque suscipit ut molestiae atque hic quibusdam ipsa optio doloribus consectetur, omnis quam et repudiandae alias autem facilis, aliquid aspernatur commodi neque excepturi eos asperiores. Voluptas quidem ipsum at consequatur, quis amet obcaecati dolorem illum earum officiis! Totam, esse. Eaque nisi ex quisquam aliquid dolores odit architecto cum adipisci placeat repellendus eum magni, non expedita! Possimus adipisci doloribus veniam quam iusto eligendi aliquam inventore nihil libero quas, aliquid suscipit necessitatibus! Autem sapiente accusamus quidem nesciunt. Perferendis error blanditiis, incidunt ratione nostrum quaerat asperiores nobis assumenda ad minima culpa!</p>
   </div>
 </template>
--- ninja-jobs/src/views/Home.vue
+++ ninja-jobs/src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">    
+    <h1>Home sweet home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quasi enim quos doloremque suscipit ut molestiae atque hic quibusdam ipsa optio doloribus consectetur, omnis quam et repudiandae alias autem facilis, aliquid aspernatur commodi neque excepturi eos asperiores. Voluptas quidem ipsum at consequatur, quis amet obcaecati dolorem illum earum officiis! Totam, esse. Eaque nisi ex quisquam aliquid dolores odit architecto cum adipisci placeat repellendus eum magni, non expedita! Possimus adipisci doloribus veniam quam iusto eligendi aliquam inventore nihil libero quas, aliquid suscipit necessitatibus! Autem sapiente accusamus quidem nesciunt. Perferendis error blanditiis, incidunt ratione nostrum quaerat asperiores nobis assumenda ad minima culpa!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum quasi enim quos doloremque suscipit ut molestiae atque hic quibusdam ipsa optio doloribus consectetur, omnis quam et repudiandae alias autem facilis, aliquid aspernatur commodi neque excepturi eos asperiores. Voluptas quidem ipsum at consequatur, quis amet obcaecati dolorem illum earum officiis! Totam, esse. Eaque nisi ex quisquam aliquid dolores odit architecto cum adipisci placeat repellendus eum magni, non expedita! Possimus adipisci doloribus veniam quam iusto eligendi aliquam inventore nihil libero quas, aliquid suscipit necessitatibus! Autem sapiente accusamus quidem nesciunt. Perferendis error blanditiis, incidunt ratione nostrum quaerat asperiores nobis assumenda ad minima culpa!</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    
   }
 }
 </script>
