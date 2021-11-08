034c2a6d651a772081db4f7f85c7f64488b47972
OlgaModified <olga@modified.dk>
2021-11-08 10:44:24 +0100

    refs in setup example

--- composition-blog/src/views/Home.vue
+++ composition-blog/src/views/Home.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="home">
-    
-    
+  <div class="home">    
+    <p ref="p">Person name is {{name}}, person's age {{ age }}</p>
+    <button @click="handleClick">Click Me!</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity'
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  /* name: '',
-  components: {
-    
-  } */
+  name: 'Home',
+  setup() {
+    //console.log('setup')
+
+    const p = ref(null)
+    let name = 'mario'
+    let age = '29'
+
+    const handleClick = ()=>{
+      console.log(p)
+    }
+    return {name, age, handleClick, p}
+  },
+  data() {
+    return {
+      
+    }
+  },
+  created(){
+    //console.log('created')
+
+  },
+  mounted(){
+    //console.log('mounted')
+
+  }
 }
 </script>
