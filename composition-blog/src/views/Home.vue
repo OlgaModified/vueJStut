47e2e76c05945a9f2a9ce692cd5e2f9e8bce6429
OlgaModified <olga@modified.dk>
2021-11-10 13:09:26 +0100

    refs vs reactive

--- composition-blog/src/views/Home.vue
+++ composition-blog/src/views/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="home">
-    <h1>Post about jobs</h1>    
-    <p ref="p"> Name:  {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click Me!</button>
-    <button @click="age++">Increment age for 1time</button>
-    <input type="text" v-model="name"/>
+    <h1>Refs</h1>    
+    <p > Our vilain for today is:  {{ heroOne.name }}. Our vilain is born {{ heroOne.age }} years ago</p>
+    <button @click="updateHero">Update Hero</button>
+     <h1>Reactive - primitive can't be updated</h1>    
+    <p > Our hero for today is:  {{ heroTwo.name }}. Our hero is born {{ heroTwo.age }} years ago</p>
   </div>
 </template>
 
 <script>
-import { ref } from '@vue/reactivity'
+import { ref, reactive } from '@vue/reactivity'
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -17,23 +17,23 @@ export default {
   name: 'Home',
   setup() {
     //console.log('setup')
-    const p = ref(null)
-    let name= ref("Mario")
-    let age = ref(30)
+    const heroOne = ref({name: 'Dracula', age: 150})
+    const heroTwo = reactive({name: 'Ironman', age: 25})
     const jobs = ref([
         {title: "Web Developer", id: 1, body: "A web developer is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server ..."},
         {title: "Software Developer", id: 1, body: "Agile software development services for ambitious organizations. Flexible partnership. We implement robust processes in software development that result in reliable solutions. 16+ years of experience. Excellent tech know-how. We offer peace of mind."}
         ])
 
-    const handleClick = ()=>{
+    const updateHero = ()=>{
      // console.log(p, p.value)
      // p.value.classList.add('test')
       //p.value.textContent = "Hejsa!"
-      name.value = 'luigi'
-      age.value = 35
+      heroOne.value.age += 500
+      heroTwo.age +=10
+      
     }
      
-    return {jobs, handleClick, p, name, age }
+    return {jobs, updateHero, heroOne, heroTwo }
   },
   data() {
     return {
