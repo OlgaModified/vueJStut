fdf64401f30eb4fa843bb64db64d0e45a9deeb64
OlgaModified <olga@modified.dk>
2021-11-10 13:41:04 +0100

    computed values

--- composition-blog/src/views/Home.vue
+++ composition-blog/src/views/Home.vue
@@ -1,52 +1,32 @@
 <template>
   <div class="home">
-    <h1>Refs</h1>    
-    <p > Our vilain for today is:  {{ heroOne.name }}. Our vilain is born {{ heroOne.age }} years ago</p>
-    <button @click="updateHero">Update Hero</button>
-     <h1>Reactive - primitive can't be updated</h1>    
-    <p > Our hero for today is:  {{ heroTwo.name }}. Our hero is born {{ heroTwo.age }} years ago</p>
+    <h1>Computed values</h1>    
+    <input type="text" v-model="search" />
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name"> Our man for today is:  {{ name }}.</div>
+    <!-- <button @click="updateHero">Update Hero</button> -->
+     
   </div>
 </template>
 
 <script>
-import { ref, reactive } from '@vue/reactivity'
+import { computed, ref } from '@vue/reactivity'
 // @ is an alias to /src
 //import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   setup() {
-    //console.log('setup')
-    const heroOne = ref({name: 'Dracula', age: 150})
-    const heroTwo = reactive({name: 'Ironman', age: 25})
-    const jobs = ref([
-        {title: "Web Developer", id: 1, body: "A web developer is a programmer who specializes in, or is specifically engaged in, the development of World Wide Web applications using a client–server ..."},
-        {title: "Software Developer", id: 1, body: "Agile software development services for ambitious organizations. Flexible partnership. We implement robust processes in software development that result in reliable solutions. 16+ years of experience. Excellent tech know-how. We offer peace of mind."}
-        ])
+    //console.log('setup')  
+    const search = ref('')  
+    const names = ref(['ironman', 'dracula', 'hulk', 'mario', 'marion', 'peach'])
 
-    const updateHero = ()=>{
-     // console.log(p, p.value)
-     // p.value.classList.add('test')
-      //p.value.textContent = "Hejsa!"
-      heroOne.value.age += 500
-      heroTwo.age +=10
-      
-    }
+    const matchingNames = computed(()=>{
+      return names.value.filter((name)=> name.includes(search.value))
+    })
      
-    return {jobs, updateHero, heroOne, heroTwo }
-  },
-  data() {
-    return {
-      
-    }
-  },
-  created(){
-    //console.log('created')
-
-  },
-  mounted(){
-    //console.log('mounted')
-
+    return {names, search, matchingNames}
   }
+  
 }
 </script>
